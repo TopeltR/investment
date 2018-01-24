f2748df56262f78000cc73f6c76915e4c6d6cb1f
Rasmus Rüngenen <r.ryngenen@gmail.com>
2018-01-24 19:25:54 +0200

    Listing file improvment(working with Big Data)

--- listing.py
+++ listing.py
@@ -1,6 +1,11 @@
 import bs4 as bs
+import datetime as dt
+import os
+import pandas as pd
+import pandas_datareader.data as web
 import pickle
 import requests
+import sys
 
 
 def save_sp500_tickers():
@@ -10,10 +15,56 @@ def save_sp500_tickers():
     tickers = []
     for row in table.findAll('tr')[1:]:
         ticker = row.findAll('td')[0].text
+        mapping = str.maketrans(".", "-")
+        ticker = ticker.translate(mapping)
         tickers.append(ticker)
     with open("sp500tickers.pickle", "wb") as f:
         pickle.dump(tickers, f)
     print(tickers)
     return tickers
 
-save_sp500_tickers()
+#save_sp500_tickers()
+
+
+def get_data_from_yahoo(reload_sp500=False):
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+    else:
+        with open("sp500tickers.pickle", "rb") as f:
+            tickers = pickle.load(f)
+
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
+
+    start = dt.datetime(2000, 1, 1)
+    end = dt.datetime(2016, 12, 31)
+
+    for ticker in tickers:
+        print(ticker)
+        try:
+            if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+                df = web.DataReader(ticker, 'yahoo', start, end)
+                df.to_csv('stock_dfs/{}.csv'.format(ticker))
+            else:
+                print('Already have {}'.format(ticker))
+        except IOError:
+            print("I/O error({0})")
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+
+
+#get_data_from_yahoo()
+
+def compile_data():
+    with open("sp500tickers.pickle", "rb") as f:
+        tickers = pickle.load(f)
+
+    main_df = pd.DataFrame()
+
+    for count, ticker in enumerate(tickers):
+        df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+        df.set_index('Date', inplace=True)
+
+        df.rename(columns={'Adj Close', ticker})
+        df.drop(['Open','High','Low','Close','Volume'], 1, inplace=True)
+
